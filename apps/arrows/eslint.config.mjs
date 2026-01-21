36c2078ade15b5c79b77e156a6e7a6cadb967bbd
Javier Lerones Gallego <javier@covr.io>
2026-01-21 15:45:52 -0500

    changes component prefixes

--- apps/arrows/eslint.config.mjs
+++ apps/arrows/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: 'arrows',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: 'arrows',
           style: 'kebab-case',
         },
       ],
